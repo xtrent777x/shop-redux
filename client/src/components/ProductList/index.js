06f5e088d4ec614c94e8bdb9cf14ba0883fb6cce
Trent Roesbery <xtrent777x@gmail.com>
2021-11-04 22:40:06 -0600

    removed gloablstate refrence form all componants pages

--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import ProductItem from '../ProductItem';
-import { useStoreContext } from '../../utils/GlobalState';
+// import { useStoreContext } from '../../utils/GlobalState';
 import { UPDATE_PRODUCTS } from '../../utils/actions';
 import { useQuery } from '@apollo/client';
 import { QUERY_PRODUCTS } from '../../utils/queries';
